bdfe916e5c982975c9ebffe563fef2f610c36d00
Diogo <Diogo.Junqueira-ext@ab-inbev.com>
2024-07-05 19:30:51 -0300

    Fix: remove @Data usages

--- src/main/java/domain/entity/Lock.java
+++ src/main/java/domain/entity/Lock.java
@@ -6,14 +6,18 @@ import domain.enums.LockStatus;
 import domain.entity.vos.lock.RunningDetailsVO;
 import domain.event.EventPublisher;
 import domain.event.impls.LockAcquiredEvent;
+import domain.repository.ILockRepository;
 import domain.repository.PersistenceContext;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 
-@Data
+@AllArgsConstructor
+@Getter
 public class Lock {
     private String id;
     private Integer version;
@@ -25,38 +29,4 @@ public class Lock {
     private Date partitionDate;
     private Object context;
     private List<String> workingSet;
-
-    public static Boolean isStopTheWorldRunning(PersistenceContext persistenceContext) {
-        return persistenceContext.lockRepository().hasStopTheWorldLockOnRunningStatus();
-    }
-
-    public static void lockCreatedAcquireOpportunity(PersistenceContext persistenceContext, String lockId) {
-        if (Boolean.TRUE.equals(Lock.isStopTheWorldRunning(persistenceContext))
-                || Boolean.TRUE.equals(persistenceContext.lockRepository().hasConcurrentProcessRunning(lockId))) {
-            return;
-        }
-
-        Lock acquiredLock = persistenceContext.lockRepository().updateToRunning(lockId);
-
-        var acquireLockEventVO = new AcquireLockEventVO(acquiredLock.getVersion(), acquiredLock.getProcess().getName(),
-                acquiredLock.getProcess().getNotificationTarget(), acquiredLock.getWorkingSet(), acquiredLock.getContext());
-
-        var lockAcquiredEventVO = new LockAcquiredEventVO(lockId, acquireLockEventVO);
-        var lockAcquiredEvent = new LockAcquiredEvent(lockAcquiredEventVO);
-
-        EventPublisher.publishEvent(lockAcquiredEvent);
-    }
-
-    public static void lockReleasedAcquireOpportunity(PersistenceContext persistenceContext, String lockId) {
-        if (Boolean.TRUE.equals(Lock.isStopTheWorldRunning(persistenceContext))) {
-            return;
-        }
-
-        var concurrentProcessesPending = persistenceContext.lockRepository().getPendingConcurrentProcessOrderedByPriority(lockId);
-        if(concurrentProcessesPending.size() > 0) {
-            concurrentProcessesPending.forEach(concurrentProcessPending -> {
-                Lock.lockCreatedAcquireOpportunity(persistenceContext, concurrentProcessPending);
-            });
-        }
-    }
 }
--- src/main/java/domain/event/DomainEvent.java
+++ src/main/java/domain/event/DomainEvent.java
@@ -3,9 +3,10 @@ package domain.event;
 import domain.enums.DomainEventType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 @AllArgsConstructor
-@Data
+@Getter
 public abstract class DomainEvent {
     private final DomainEventType type;
     private final Object content;
